ac4656ef47fedfd059354e69f116ca6dcd4c3d94
blerim2 <blerimballiu19@gmail.com>
2024-01-24 13:17:09 +0000

    calculator

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
-    if __name__ == '__main__':
-    args = sys.argv[:1]
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
     num_args = len(args)
     if num_args == 0:
-    print("0 arguments")
+        print("0 arguments.")
     elif num_args == 1:
-    print("1 arguments")
-    else
-     print(num_args, "arguments:")
+        print("1 argument:")
+        print("1:", args[0])
+    else:
+        print(num_args, "arguments:")
         for i, arg in enumerate(args):
             print(i+1, ":", arg)
