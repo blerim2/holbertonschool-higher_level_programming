f86cd7ba16805b6508b000b5f88adce6a47bb05b
blerim2 <blerimballiu19@gmail.com>
2024-01-29 12:59:09 +0000

    keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for keys in sorted(a_dictionary.key())
+    for keys in sorted(a_dictionary.keys()):
     print(f"{key}: {a_dictionary[key]}")
