959ec4618a21d9e55a71ae87f657cc3b4cbbd562
blerim2 <blerimballiu19@gmail.com>
2024-02-07 22:35:18 +0000

    inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+'''
+comment
+'''
+
+
 def lookup(obj):
+    '''
+    Returns the list of available attributes and methods of an object
+    '''
     return dir(obj)
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-    def is_kind_of_class(obj, a_class):
+"""Defines a class and inherited class-checking function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an object is an instance of, or if the object
+    is an instance of a class that inherited from,
+    the specified class."""
     return isinstance(obj, a_class)
