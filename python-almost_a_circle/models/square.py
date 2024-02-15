bef065fdf24f0981b220809cff40aa563ee1c7a8
blerim2 <blerimballiu19@gmail.com>
2024-02-15 11:25:15 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,85 +1,31 @@
 #!/usr/bin/python3
-'''base class'''
-from .base import Base
+'''class Square'''
+from .rectangle import Rectangle
 
 
-class Rectangle(Base):
-    '''constructor'''
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        '''area of rectangle'''
-        return self.__height * self.__width
-
-    def display(self):
-        '''display of rectangle'''
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x, end="")
-            print("#" * self.width)
+class Square(Rectangle):
+    '''class Square'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Constructor'''
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''str'''
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
-                f"{self.__width}/{self.__height}")
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        '''size of this square'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        '''update rectangle'''
-        attributes = ['id', 'width', 'height', 'x', 'y']
+        '''update'''
+        attributes = ['id', 'size', 'x', 'y']
         for i in range(len(args)):
             if i < len(attributes):
                 setattr(self, attributes[i], args[i])
@@ -90,8 +36,7 @@ class Rectangle(Base):
         '''dictionary'''
         return {
             'id': self.id,
-            'width': self.width,
-            'height': self.height,
             'x': self.x,
+            'size': self.size,
             'y': self.y
         }
