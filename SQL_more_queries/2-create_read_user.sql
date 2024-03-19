466f550c393c6d30a6d32c853d9bfc3901fcac4b
blerim2 <blerimballiu19@gmail.com>
2024-03-19 12:44:47 +0100

    privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- creating database hbtn_0d_2 and user user_0d_2 which should have only SELECT privilege.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2_pwd'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
