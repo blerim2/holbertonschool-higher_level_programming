6d168a6174b1c7f19c69d8763dab96bf2d623e77
blerim2 <blerimballiu19@gmail.com>
2024-03-19 12:53:16 +0100

    privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,17 +1,5 @@
--- Connect to MySQL server as root (you can replace 'root' with your actual MySQL username)
-mysql -u root -p
-
--- Create the database 'hbtn_0d_2' if it doesn't exist
+-- creating database hbtn_0d_2 and user user_0d_2 which should have only SELECT privilege.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user 'user_0d_2' with the specified password
+CREATE USER 'user_0d_2_pwd'@'localhost' IDENTIFIED BY 'user_0d_2';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2_pwd'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on the database 'hbtn_0d_2' to the user 'user_0d_2'
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
--- Exit MySQL
-EXIT;
