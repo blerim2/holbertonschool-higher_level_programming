fa00b5fecbb3aed9fb7b2d88f2f0c1ed41193a17
blerim2 <blerimballiu19@gmail.com>
2024-03-20 10:50:17 +0100

    privileges

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,7 @@
---This script will create the database hbtn_0d_usa if it does not already exist. 
--- It then selects this database with the USE statement.
+-- Create the states table if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
