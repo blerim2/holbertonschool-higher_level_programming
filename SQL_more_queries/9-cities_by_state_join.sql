5d6842164600098f413785067bd7049b62822ae2
blerim2 <blerimballiu19@gmail.com>
2024-03-21 10:24:14 +0100

    privileges

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa
+-- Each record displays: cities.id - cities.name - states.name
+-- Results are sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
