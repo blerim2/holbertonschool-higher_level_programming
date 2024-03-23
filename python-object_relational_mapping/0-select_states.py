5b888b427288eafb9d2d4eea626c41ffc00c5acf
blerim2 <blerimballiu19@gmail.com>
2024-03-23 20:52:26 +0000

    sql

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
-"""List states from the databases hbtn_0e_0_usa"""
-
+"""Module that lists all states from mySQL database"""
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
-    """List all states from the database hbtn_0e_0_usa"""
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetchall()
+def list_states (username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host='localhost',\
+            port=3306,\
+            user=username,\
+            passwd=password,\
+            db=database)
+    cursor = db.cursor()
+
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
+
+    # Print the results
     for row in rows:
         print(row)
-    cur.close()
+
+    # Close the database connection
     db.close()
+
+# Example usage
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
