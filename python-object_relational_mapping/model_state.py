a320f67e26775e21d24c740ea7e6ba8a68b9480a
blerim2 <blerimballiu19@gmail.com>
2024-03-23 21:08:40 +0000

    sql

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""
+The class definition of a State
+Instance Base = declarative_base()
+"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class City(Base):
-    """City class."""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
